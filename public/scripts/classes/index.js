4bfb44cde0c17ffb192ce12a5cc6fec8cb978a2c
Muaawiyah Dadabhay <100700962+meneerfrikkie@users.noreply.github.com>
2024-04-23 21:02:32 +0200

    feat: Add Enter Button Functionality on Welcome Screen

---
+++ public/scripts/classes/index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const enterWebsiteBtn = document.getElementById("enterWebsiteBtn");
+
+    enterWebsiteBtn.addEventListener("click", function() {
+        alert("Button Clicked!");
+    });
+});
