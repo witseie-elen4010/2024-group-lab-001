79f0e649d7ac4ea3ddc8cd4a489d19bec13124b6
Muaawiyah Dadabhay <100700962+meneerfrikkie@users.noreply.github.com>
2024-04-24 20:51:09 +0200

    test: Welcome(Home) Page Tests

---
+++ tests/home.test.js
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('Navigate to HOME and then to ACCOUNT', async ({ page }) => {
+    // Navigate to the homepage
+    await page.goto('http://localhost:3000/');
+    // Click on the HOME link
+    await page.click('text=HOME');
+    // Check if the URL contains '/home'
+    expect(page.url()).toContain('/');
+    
+    // Click on the ACCOUNT link
+    await page.click('text=ACCOUNT');
+    // Check if the URL contains '/account'
+    expect(page.url()).toContain('/account');
+});
