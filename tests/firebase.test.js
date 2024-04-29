198705e220779728cffc69d5ae4f32c6fa6d3147
MRaeesD <mraees.dindar@gmail.com>
2024-04-29 15:02:31 +0200

    test: Add test for redirect after login

--- tests/firebase.test.js
+++ tests/firebase.test.js
@@ -12,3 +12,15 @@ test('Signup fields can be filled', async ({ page }) => {
     await page.fill('input[placeholder="Create a password"]', 'alexalex');
     await page.fill('input[placeholder="Confirm a password"]', 'alexalex');
 });
+
+test('Login redirects user to lobby', async ({ page }) => {
+    await page.goto('http://localhost:3000/account');
+    // Check if the signup form is active
+    //await expect(page.$('.account-container.active .login')).resolves.toBeTruthy();
+    // Fill the signup fields with test data
+    await page.fill('input[placeholder="Enter your username"]', 'alex@minecraft.test');
+    await page.fill('input[placeholder="Enter your password"]', 'alexalex');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    expect(page.url()).toContain('/lobby');
+});
