9d843823b219b6fdf189f8e852d5facd0a2ce930
MRaeesD <mraees.dindar@gmail.com>
2024-05-06 22:25:52 +0200

    feat: Add username to databse on guest login

--- public/scripts/classes/firebase.js
+++ public/scripts/classes/firebase.js
@@ -99,10 +99,12 @@ const monitorAuthState = (req, res) => {
 async function addUserToDB(myEmail, myUsername) {
 
     try{
-        userInformation = doc(firestore, `users/${myEmail}`)
+        const userUID = getUserUID();
+        userInformation = doc(firestore, `users/${userUID}`)
         const userData ={
             username: myUsername,
-            email: myEmail
+            email: myEmail,
+            uid: userUID
         }
         setDoc(userInformation, userData);
 
@@ -131,9 +133,10 @@ const loginEmailPassword = async(myEmail, myPassword, req, res) => {
 
 // Function to get the username of the current user
 const getUsername = async function() {
-    const userEmail = getUserEmail();
-    if (userEmail !== null) {
-        const userInformation = doc(firestore, `users/${userEmail}`);
+    //const userEmail = getUserEmail();
+    const userUID = getUserUID();
+    if (userUID !== null) {
+        const userInformation = doc(firestore, `users/${userUID}`);
         const mySnapshot = await getDoc(userInformation);
         if(mySnapshot.exists()){
             const docData = mySnapshot.data();
@@ -157,12 +160,22 @@ const getUserEmail = function() {
     }
 }
 
+const getUserUID = function() {
+    const user = auth.currentUser;
+    if (user !== null) {
+        return user.uid;
+    } else {
+        return null;
+    }
+}
+
 const loginGuest = async(myUsername, req, res) => {
     console.log("Guest sign in with username: " + myUsername);
     try {
         const userCredential = await signInAnonymously(auth);
         console.log('Guest User has logged in');
         monitorAuthState(req, res);
+        addUserToDB(null, myUsername);
     } catch (error) {
         console.log('There was an error logging in guest');
         console.log(error);
